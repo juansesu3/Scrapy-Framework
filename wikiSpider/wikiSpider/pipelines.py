eee0e260e130e5412d7d96da558b9aa7bf3076e1
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-04-13 15:26:02 +0200

    pipelines

--- wikiSpider/wikiSpider/pipelines.py
+++ wikiSpider/wikiSpider/pipelines.py
@@ -7,7 +7,19 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from datetime import datetime
+from wikiSpider.items import Article
+from string import whitespace
 
-class WikispiderPipeline:
-    def process_item(self, item, spider):
-        return item
+class WikispiderPipeline(object):
+    def process_item(self, article, spider):
+
+        dateStr = article['lastUpdated']
+        article['lastUpdated'] = article['lastUpdated'].replace('This page was last edited on', '')
+        article['lastUpdated'] = article['lastUpdated'].strip()
+        #article['lastUpdated'] = datetime.strptime(dateStr, '%d %B %Y, at %H:%M.')
+
+        article['text'] = [line for line in article['text']
+                           if line not in whitespace]
+        article['text'] = ''.join(article['text'])
+        return article
